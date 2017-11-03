e2f54266839f8b4e30be697dc5f6446c8bf29e0e
terryblue99 <terryblue@comcast.net>
2017-11-03 10:22:09 -0500

    first commit

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,7 +1,38 @@
 class WatchesController < ApplicationController
-
+	before_action :set_watch, only: [:show, :edit, :update, :destroy]
+	
 	def index
+		binding.pry
+	end
 
+	def rows
+	  	binding.pry
+	end
+
+	def show
+	    binding.pry
+	end
+
+	def new
+		binding.pry
+	end
+
+	def create
+		binding.pry
+	end
+
+	def update
+		binding.pry
+	end
+
+	def destroy
+		binding.pry
+	end
+
+	private
+
+	def set_watch
+		@watch = Watch.find_by_id(params[:id])
 	end	
 
-end	
+end
