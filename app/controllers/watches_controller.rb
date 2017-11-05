751f0c9ea225adad041dad86391831601049c8c0
terryblue99 <terryblue@comcast.net>
2017-11-05 01:48:01 -0500

    Added sign in validations for watches controller

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,17 +1,22 @@
 class WatchesController < ApplicationController
+	before_filter :authenticate_user!
 	before_action :set_watch, only: [:show, :edit, :update, :destroy]
 	
 	def index
-		if @user = User.find_by(email: current_user.email)
-		    @watches_for_page_display = @user.watches.all
-		  	if session[:rows] # Selection made of how many watches to display on each page
-		    	@watches = @user.watches.paginate(:page => params[:page], :per_page => session[:rows]).order(:maker, :name)
-		  	else # First time displaying watches
-		    	@watches = @user.watches.paginate(:page => params[:page], :per_page => 15).order(:maker, :name)
-		  	end
+		if user_signed_in?
+			if @user = User.find_by(email: current_user.email)
+			    @watches_for_page_display = @user.watches.all
+			  	if session[:rows] # Selection made of how many watches to display on each page
+			    	@watches = @user.watches.paginate(:page => params[:page], :per_page => session[:rows]).order(:maker, :name)
+			  	else # First time displaying watches
+			    	@watches = @user.watches.paginate(:page => params[:page], :per_page => 15).order(:maker, :name)
+			  	end
+			else
+				redirect_to log_in_path, alert: "Please Log In to continue!"
+			end
 		else
-			redirect_to log_in_path
-		end
+			redirect_to log_in_path, alert: "Please Log In to continue!"
+		end	
 	end
 
 	def rows
@@ -19,62 +24,81 @@ class WatchesController < ApplicationController
 	end
 
 	def show
-	    if !@watch   
-	      	redirect_to watches_path, alert: "The watch was not found!"
-	    end
+		if user_signed_in?
+		    if !@watch   
+		      	redirect_to watches_path, alert: "The watch was not found!"
+		    end
+		else
+			redirect_to log_in_path, alert: "Please Log In to continue!" 
+		end	  
 	end
 
 	def new
-		@watch = Watch.new
-		@all_complications = Complication.all
-		@watch_complications = @watch.complications_watches.build
+		if user_signed_in?
+			@watch = Watch.new
+			@all_complications = Complication.all
+			@watch_complications = @watch.complications_watches.build
+		else
+			redirect_to log_in_path, alert: "Please Log In to continue!"
+		end	
 	end
 
 	def create
-		@watch = Watch.create(watch_params)
-		if @watch.errors.full_messages.size > 0
-			session[:watch_errors] = @watch.errors.full_messages
-	      	redirect_to new_watch_path
-	    else
-	   		current_user.watches << @watch
-	   		params[:complications][:id].each do |complication|
-	   			if !complication.empty?
-	   				@watch.complications_watches.build(complication_id: complication).save
-	   			end	
-	   		end	
-	      	redirect_to watch_path(@watch), notice: "The watch was successfully saved!"
-		end
-
+		if user_signed_in?
+			@watch = Watch.create(watch_params)
+			if @watch.errors.full_messages.size > 0
+				session[:watch_errors] = @watch.errors.full_messages
+		      	redirect_to new_watch_path
+		    else
+		   		current_user.watches << @watch
+		   		params[:complications][:id].each do |complication|
+		   			if !complication.empty?
+		   				@watch.complications_watches.build(complication_id: complication).save
+		   			end	
+		   		end	
+		      	redirect_to watch_path(@watch), notice: "The watch was successfully saved!"
+			end
+		else
+			redirect_to log_in_path, alert: "Please Log In to continue!"
+		end	
 	end
 
 	def update
-		@watch.update(watch_params)
-		if @watch.errors.full_messages.size > 0
-			session[:watch_errors] = @watch.errors.full_messages
-	      	redirect_to edit_watch_path
-	    else
-	    	params[:complications][:id].each do |complication|
-	   			if !complication.empty?
-	   				if !@watch.complications_watches.detect {|cw| cw.complication_id == complication.to_i}
-			   				@watch.complications_watches.build(complication_id: complication).save
-			   				@cw = ComplicationsWatch.last
-			   				@cw.complication_description = Complication.find_by(id: complication).description
-			   				@cw.save
-			   		end		
-	   			end
-	   		end
-	     	redirect_to watch_path, notice: "The watch was successfully edited!"
-	    end
+		if user_signed_in?
+			@watch.update(watch_params)
+			if @watch.errors.full_messages.size > 0
+				session[:watch_errors] = @watch.errors.full_messages
+		      	redirect_to edit_watch_path
+		    else
+		    	params[:complications][:id].each do |complication|
+		   			if !complication.empty?
+		   				if !@watch.complications_watches.detect {|cw| cw.complication_id == complication.to_i}
+				   				@watch.complications_watches.build(complication_id: complication).save
+				   				@cw = ComplicationsWatch.last
+				   				@cw.complication_description = Complication.find_by(id: complication).description
+				   				@cw.save
+				   		end		
+		   			end
+		   		end
+		     	redirect_to watch_path, notice: "The watch was successfully edited!"
+		    end
+		else
+			redirect_to log_in_path, alert: "Please Log In to continue!"
+		end
 	end
 
 	def destroy
-		if !@watch   
-	      	redirect_to watches_path, alert: "The watch was not found!"
-	    else
-	      	watch_name = @watch.name	
-	      	@watch.delete
-	      	redirect_to watches_path, notice: "'#{watch_name}' has been deleted!"
-	    end
+		if user_signed_in?
+			if !@watch   
+		      	redirect_to watches_path, alert: "The watch was not found!"
+		    else
+		      	watch_name = @watch.name	
+		      	@watch.delete
+		      	redirect_to watches_path, notice: "'#{watch_name}' has been deleted!"
+		    end
+		else
+			redirect_to log_in_path, alert: "Please Log In to continue!"
+		end
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  	
+	devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
-  root 'watches#index'
-
-  resources :watches
-
-  get 'watches/:id/rows', to: 'watches#rows', as: 'rows'
+	authenticate :user do  
+	  root 'watches#index'
+	  resources :watches
+	  get 'watches/:id/rows', to: 'watches#rows', as: 'rows'
+	end  
   
 end
