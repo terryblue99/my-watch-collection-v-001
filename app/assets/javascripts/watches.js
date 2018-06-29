a188d9042bf1ff1fbfc54e8324e01de02d2389e8
terryblue99 <terryblue@comcast.net>
2018-06-28 20:20:49 -0500

    Replaced with jQuery/JSON processing

--- app/assets/javascripts/watches.js
+++ app/assets/javascripts/watches.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
   attachListeners();
-});
+})
 
 function attachListeners() {
 
-  $('a.load_watches').on('click', function(e) {
-		$.ajax({
-	      url: this.href,
-	      dataType: 'script'
-	    })
+  	$('a.load_watches').on('click', function(e) {
+
+ 	$.getJSON(this.href).success(function(json){
+ 		
+			// clear the UL html (in case there were previous watches)
+		  	let $ul = $("div.watches ul")
+		  	$ul.html("") // emptied the UL
+		  	
+		  	//iterate over each watch within json
+		  	json.forEach(function(watch){
+		  		// with each watch data, append an LI to the UL with the watch content
+		  		$ul.append(`<li class="text-success">${watch.watch_maker}: <b><a href="/watches/${watch.id}">${watch.watch_name}</a></b></li>`)
+			})
+			
+		})
 		e.preventDefault()
 	})
 }
