0a1f0d3982cb67d8048d56d4ebb35ce9856acc0f
terryblue99 <terryblue@comcast.net>
2018-07-02 17:24:45 -0500

    Added code to clear the UL and changed call to a jquery get call

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,13 +1,13 @@
 
 $(function() {
-	debugger
+	
 	$(".pagination a").click(function(e) {
-		debugger
-		// $(".pagination").html("Page is loading...")
-	    $.ajax({
-	    	url: this.href,
-	    	dataType: "script"
-	    });
+		
+		// clear the UL html (in case there were previous watches)
+	  	let $ul = $("div.watches ul")
+	  	$ul.html("") // emptied the UL
+
+	    $.get(this.href, null, null, "script")
 
 		e.preventDefault()
 	})
