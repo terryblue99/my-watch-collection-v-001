3e5b546352c44f46d25d416d519b5726d44c0095
terryblue99 <terryblue@comcast.net>
2018-07-02 02:34:36 -0500

    Added layout_helper.rb for javascript

---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,8 @@
+module LayoutHelper
+  
+  def javascript(*args)
+    args = args.map { |arg| arg == :defaults ? arg : arg.to_s }
+    content_for(:head) { javascript_include_tag(*args) }
+  end
+  
+end
