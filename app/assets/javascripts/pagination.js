e913861afff77fb3c16b3c60a602632222031131
terryblue99 <terryblue@comcast.net>
2018-07-03 00:39:26 -0500

    Removed commented out code

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -2,14 +2,9 @@
 $(function() {
 	
 	$(document).on("click", ".pagination a", function(e) {
-		
-		// clear the div html (in case there were previous watches)
-	  	// let $div = $("div.watches")
-	  	// $div.html("") // emptied the div
-
+	  	
 	    $.get(this.href, null, null, "script")
 
 		e.preventDefault()
 	})
-
 })
