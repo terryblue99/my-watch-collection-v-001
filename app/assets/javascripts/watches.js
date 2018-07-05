cc429288e7311e313ce1136e9b301b5394ff33c3
terryblue99 <terryblue@comcast.net>
2018-07-04 23:31:10 -0500

    Updated the pagination code

--- app/assets/javascripts/watches.js
+++ app/assets/javascripts/watches.js
@@ -10,18 +10,19 @@ function attachListeners() {
 
 		// get watches stored in the database		
 		$.getJSON(this.href).success(function(json){
-			
+	
 			// clear the div html (in case there were previous watches)
 		  	$("div.watches").html("")
-		  	
+
 		  	// iterate over each watch within json
 		  	json.forEach(function(watch){
 		  		// append each watch data to the watches div
 		  		$("div.watches").append(`<h5 class="text-success">${watch.watch_maker}: <b><a href="/watches/${watch.id}">${watch.watch_name}</a></b></h5>`)
 			})
+
 			// append the pagination process to each page
-			$("div.watches").append("<%= j ("#{render 'paginate'}").html_safe %>")
-			
+			$("div.watches").html("<%= j (render("paginate")) %>")
+				
 		})
 		e.preventDefault()
 	})
