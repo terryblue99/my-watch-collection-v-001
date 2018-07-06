d0644599dd40b067fc11797b4f8ad8f686b2f61e
terryblue99 <terryblue@comcast.net>
2018-07-06 18:04:08 -0500

    Updated pagination process

--- app/assets/javascripts/watches.js
+++ app/assets/javascripts/watches.js
@@ -13,16 +13,19 @@ function attachListeners() {
 	
 			// clear the div html (in case there were previous watches)
 		  	$("div.watches").html("")
-
+		  	$("div.watches_paginate").html("")
+		  	
 		  	// iterate over each watch within json
 		  	json.forEach(function(watch){
 		  		// append each watch data to the watches div
 		  		$("div.watches").append(`<h5 class="text-success">${watch.watch_maker}: <b><a href="/watches/${watch.id}">${watch.watch_name}</a></b></h5>`)
 			})
-
 			// append the pagination process to each page
-			// $("div.watches").html("<%= j (render("paginate")) %>")
-				
+			var $script = document.createElement( "script" )
+			$script.type = "text/javascript"
+			$script.innerHTML = '$("div.watches").append("<%= j (render("paginate")) %>")'
+			$("div.watches_paginate").append( `'${$script}'` )
+
 		})
 		e.preventDefault()
 	})
