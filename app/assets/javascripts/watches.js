a0d5c6d145227128ba1b25f247bb59a69a57c3f4
terryblue99 <terryblue@comcast.net>
2018-07-08 17:11:36 -0500

    Added code to capture this.href

--- app/assets/javascripts/watches.js
+++ app/assets/javascripts/watches.js
@@ -7,36 +7,28 @@ function attachListeners() {
 
 	// check for when pagination links are clicked
 	$(document).on("click", ".pagination a", function(e) {
+		
+		$thisHref = this.href
 
 		// get watches stored in the database		
 		$.getJSON(this.href).success(function(json){
 			$count = 0
 			$jsonLth = json.length
-			// clear the div html (in case there were previous watches)
+			// clear the div html's of previous watches data
 		  	$(".watches").html("")
 		  	$(".watches_paginate").html("")
-		  	debugger
-		  	// iterate over each watch within json
+		  	
+		  	// iterate over each watch within json and add to the DOM
 		  	json.forEach(function(watch){
 		  		// append each watch data to the watches div
 		  		$(".watches").append(`<h5 class="text-success">${watch.watch_maker}: <b><a href="/watches/${watch.id}">${watch.watch_name}</a></b></h5>`)
 				$count += 1
-				if ($count == json.length) {
-					$.get("/", null, null, "script")
+				// if this is the last watch, execute the index.js.erb file 
+				// in the watches view to set pagination entries 
+				if ($count == $jsonLth) {
+					$.get($thisHref, null, null, "script")
 				}
 			})
-		  	debugger
-			// alert("What the heck!!")
-			// $("div.watches_paginate").html("#{j (render 'paginate')}")
-			// debugger
-			// // append the pagination process to each page
-			// var $script = document.createElement( "script" )
-			// $script.type = "text/javascript"
-			// $script.innerHTML = '$("div.watches").append("<%= j (render("paginate")) %>")'
-			// debugger
-			// $("div.watches_paginate").append( $script.toString() )
-			// debugger
-			// '[object HTMLScriptElement]'
 		})
 		e.preventDefault()
 	})
