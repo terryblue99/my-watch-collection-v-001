ad7aeee5cc7cbdeb8f096dbbcd952d66aa5edc47
terryblue99 <terryblue@comcast.net>
2018-07-11 12:10:51 -0500

    Created pagination function and saved this.href in the attachListeners function

--- app/assets/javascripts/watches.js
+++ app/assets/javascripts/watches.js
@@ -5,32 +5,36 @@ $(function() {
 
 function attachListeners() {
 
-	// check for when pagination links are clicked
-	$(document).on("click", ".pagination a", function(e) {
-		
-		$thisHref = this.href
-
-		// get watches stored in the database		
-		$.getJSON(this.href).success(function(json){
-			// clear the div html's of previous watches data
-		  	$(".watches").html("")
-		  	$(".watches_paginate").html("")
-		  	// iterate over each watch within json and add to the DOM
-		  	json.forEach(function(watch){
-		  		// append each watch data to the watches div
-		  		$(".watches").append(`<h5 class="text-success">${watch.watch_maker}: <b><a href="/watches/${watch.id}">${watch.watch_name}</a></b></h5>`)
-			})
-			// execute the related js.erb file in the watches view
-			// to set pagination entries (index.js.erb / most_maker.js.erb)
-			$.get($thisHref, null, null, "script")
-		})
-		e.preventDefault()
+	$(document).on("click", ".pagination a", function(e) {	
+		$href = this.href
+		pagination(e, $href)
 	})
 
-	$("a.show_watch").on('click', () => showWatch())
+	$("a.show_watch").on('click', function() {
+		$href = this.href
+		showWatch($href)
+	})
 }
 
-function showWatch() {
-	alert("Show watch!")
+function pagination(e, $href) {
+	// get watches stored in the database		
+	$.getJSON($href).success(function(json){
+		// clear the div html's of previous watches data
+	  	$(".watches").html("")
+	  	$(".watches_paginate").html("")
+	  	// iterate over each watch within json and add to the DOM
+	  	json.forEach(function(watch){
+	  		// append each watch data to the watches div
+	  		$(".watches").append(`<h5 class="text-success">${watch.watch_maker}: <b><a href="/watches/${watch.id}">${watch.watch_name}</a></b></h5>`)
+		})
+		// execute the related js.erb file in the watches view
+		// to set pagination entries (index.js.erb / most_maker.js.erb)
+		$.get($href, null, null, "script")
+	})
 	e.preventDefault()
+}
+
+function showWatch($href) {
+	debugger
+	
 }
