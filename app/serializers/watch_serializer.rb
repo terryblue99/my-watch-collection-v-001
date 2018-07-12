cd5167eb0f59b902f3984dc690c8ddc892b319c9
terryblue99 <terryblue@comcast.net>
2018-07-12 18:11:59 -0500

    Added 'has_many :complications

--- app/serializers/watch_serializer.rb
+++ app/serializers/watch_serializer.rb
@@ -1,3 +1,4 @@
 class WatchSerializer < ActiveModel::Serializer
   attributes :id, :watch_name, :watch_maker, :movement, :band, :model_number, :water_resistance, :date_bought
+  has_many :complications
 end
