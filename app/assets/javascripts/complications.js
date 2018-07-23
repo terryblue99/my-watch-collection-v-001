904c39887d5f29beee005fbf9c01e42e3f73fee4
terryblue99 <terryblue@comcast.net>
2018-07-23 15:02:51 -0500

    Added complications.js

---
+++ app/assets/javascripts/complications.js
@@ -0,0 +1,49 @@
+
+// handlebars greater than helper
+Handlebars.registerHelper('gt', function( a, b ){
+	var next =  arguments[arguments.length-1];
+	return (a > b) ? next.fn(this) : next.inverse(this);
+})
+
+$(function() {
+  compListeners();
+})
+
+function compListeners() {
+
+	$(document).on("click", "a.load_complications", function(e) {	
+
+		$href = this.href
+		// handlebar process
+		let templateSource = $("#complications").html()
+		let template = Handlebars.compile(templateSource)
+
+		loadComplications(e, $href, template)
+	})
+
+	$(document).on("click", "a.back", function(e){
+		// navigate to previous page when 'back' link clicked
+		parent.history.back()
+		e.preventDefault()
+	})
+
+}
+
+function loadComplications(e, $href, template) {
+
+	$.getJSON($href)
+	.success(function(json) {
+		// load watch complications via handlebars template
+		$(".complications").html(template(json))
+	})
+	.error(function(jqxhr, textStatus, error){
+	    showError(jqxhr, textStatus, error)
+	})
+	e.preventDefault()
+}
+
+function showError(jqxhr, textStatus, error) {
+
+	let err = textStatus + ', ' + error
+	alert("Request Failed: " + err)
+}
