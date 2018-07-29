d391aa9e4ea6c0d3c826cae03c4472a355435dc7
terryblue99 <terryblue@comcast.net>
2018-07-29 00:27:17 -0500

    Added comment

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,5 +1,5 @@
 module LayoutHelper
-  
+  # used by will_paginate
   def javascript(*args)
     args = args.map { |arg| arg == :defaults ? arg : arg.to_s }
     content_for(:head) { javascript_include_tag(*args) }
