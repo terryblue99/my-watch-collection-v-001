16cbb050509a80df82a6376785c8c646285c704f
terryblue99 <terryblue@comcast.net>
2018-08-14 23:04:57 -0500

    Added self.find_maker method

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -72,6 +72,14 @@ class Watch < ApplicationRecord
     	end
  	end
 
+ 	def self.find_maker(current_user, maker)
+  	# Find a maker and their watches
+		find_maker_array = current_user.watches.select { |w| w.watch_maker == maker }
+		# sorts by watch name ascending
+		find_maker_array = find_maker_array.sort_by(&:watch_name)
+
+ 	end
+
  	def self.retrieve_most_maker(current_user)
   	# Find the maker of most of the watches, and the watches
 
