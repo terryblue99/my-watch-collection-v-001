ebe367d35d6b8207a4a4feff1bd600b1c6f13f3a
terryblue99 <terryblue@comcast.net>
2018-08-14 23:17:14 -0500

    Added find_maker route

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
 	  get 'watches/:id/rows', to: 'watches#rows', as: 'rows'
 	  get '/watches/:id/most_maker', to: 'watches#most_maker', as: 'most_maker'
+	  get '/watches/:id/find_maker', to: 'watches#find_maker', as: 'find_maker'
 
 	  get '/watches/:id/newest_watches', to: 'watches#newest_watches', as: 'newest_watches'
 	  get 'complications/:id/description', to: 'complications#description', as: 'description'
