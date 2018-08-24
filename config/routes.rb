56c3ec218c038c5527e790ebfc50ce7b1d1b2ba4
terryblue99 <terryblue@comcast.net>
2018-08-23 20:34:19 -0500

    Modified watches routes and added users delete route

--- config/routes.rb
+++ config/routes.rb
@@ -15,15 +15,18 @@ Rails.application.routes.draw do
 
 	  end
 
-	  get 'watches/:id/rows', to: 'watches#rows', as: 'rows'
-	  get '/watches/:id/most_maker', to: 'watches#most_maker', as: 'most_maker'
-	  get '/watches/:id/search_watches', to: 'watches#search_watches', as: 'search_watches'
-	  get '/watches/:id/find_maker', to: 'watches#find_maker', as: 'find_maker'
+	  get 'users/:id/delete_user', to: 'users#delete_user', as: 'delete_user'
+	  post 'users/:id/delete_user', to: 'users#delete_user'
 
-	  get '/watches/:id/newest_watches', to: 'watches#newest_watches', as: 'newest_watches'
+	  get 'watches/:id/rows', to: 'watches#rows', as: 'rows'
+	  get 'watches/:id/most_maker', to: 'watches#most_maker', as: 'most_maker'
+	  get 'watches/:id/search_watches', to: 'watches#search_watches', as: 'search_watches'
+	  get 'watches/:id/find_maker', to: 'watches#find_maker', as: 'find_maker'
+
+	  get 'watches/:id/newest_watches', to: 'watches#newest_watches', as: 'newest_watches'
 	  get 'complications/:id/description', to: 'complications#description', as: 'description'
 	  get 'complications/:id/delete_list_comp', to: 'complications#delete_list_comp', as: 'delete_list_comp'
-	  post 'complications/:id/delete_list_comp', to: 'complications#delete_list_comp', as: 'delete_post'
+	  post 'complications/:id/delete_list_comp', to: 'complications#delete_list_comp'
 
 	end
 
