f1aeff4d57dbdbc2f1ce7b1d216ed40e470e614d
terryblue99 <terryblue@comcast.net>
2018-08-30 16:12:09 -0500

    Added comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   require "will_paginate/array"
 
+  # An attacker may trick the users of a web application into executing actions of the attacker's choosing
+  # Protect the app from this Cross-Site Request Forgery (CSRF) [ https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF) ]
   protect_from_forgery with: :exception
+
   before_action :authenticate_user!
 
 end
