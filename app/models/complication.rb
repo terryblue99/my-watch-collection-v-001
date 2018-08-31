49d14d0a7407001cbcd8366da9b8cf8a6555543a
terryblue99 <terryblue@comcast.net>
2018-08-31 15:06:35 -0500

    Added validation for presence of both :complication_name and :complication_description

--- app/models/complication.rb
+++ app/models/complication.rb
@@ -2,8 +2,16 @@ class Complication < ApplicationRecord
   has_many :complications_watches
   has_many :watches, through: :complications_watches
 
-  validates :complication_name, presence: true
   validates :complication_name, uniqueness: true
-  validates :complication_description, presence: true
+  validates :complication_name, presence: true, if: :complication_description_present?
+  validates :complication_description, presence: true, if: :complication_name_present?
+
+  def complication_name_present?
+    complication_name != ""
+  end
+
+  def complication_description_present?
+    complication_description != ""
+  end
 
 end
