d9f5cfd266aec9e2b1615dc6831457fd541146c2
terryblue99 <terryblue@comcast.net>
2018-09-08 15:00:27 -0500

    Added :case_measurement to strong params

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -250,7 +250,7 @@ class WatchesController < ApplicationController
 	end
 
 	def watch_params
-    	# params hash keys
+    	# params hash keys (strong params)
     	params.require(:watch).permit(
     		:watch_name,
     		:watch_maker,
@@ -260,6 +260,7 @@ class WatchesController < ApplicationController
     		:water_resistance,
     		:date_bought,
     		:cost,
+    		:case_measurement,
     		:watch_image,
     		complications_attributes: [:complication_name, :complication_description]
     	)
